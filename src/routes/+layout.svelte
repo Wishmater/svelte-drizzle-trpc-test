e09d123fe3f18abfed57d54bbb8c288d6788ae2b
David Alcalde <davidtheindestructible@gmail.com>
2025-01-29 14:46:34 -0500

    Example uses of tooltip and alert dialog in posts delete button.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import { page } from '$app/stores';
 	import { checkForServerToastMessage } from '$lib/client/util/toast_message';
 	import { route } from '$lib/ROUTES';
+	import * as Tooltip from '$lib/client/components/ui/tooltip/index.js';
 
 	let { children } = $props();
 
@@ -21,22 +22,24 @@
 
 <ModeWatcher />
 <Toaster />
-<div class="flex min-h-screen flex-col items-center bg-background font-sans">
-	<header
-		class="mb-8 flex h-20 w-full flex-row content-center items-center justify-center border-b-2"
-	>
-		<a href={route('/')}>
-			<h1>SvelteKit FullStack Test</h1>
-		</a>
-	</header>
+<Tooltip.Provider delayDuration={0}>
+	<div class="flex min-h-screen flex-col items-center bg-background font-sans">
+		<header
+			class="mb-8 flex h-20 w-full flex-row content-center items-center justify-center border-b-2"
+		>
+			<a href={route('/')}>
+				<h1>SvelteKit FullStack Test</h1>
+			</a>
+		</header>
 
-	<div class="flex flex-grow flex-col items-center">
-		{@render children()}
+		<div class="flex flex-grow flex-col items-center">
+			{@render children()}
+		</div>
+
+		<footer
+			class="mt-8 flex h-32 w-full flex-row content-center items-center justify-center border-t-2"
+		>
+			<ThemeSwitcher></ThemeSwitcher>
+		</footer>
 	</div>
-
-	<footer
-		class="mt-8 flex h-32 w-full flex-row content-center items-center justify-center border-t-2"
-	>
-		<ThemeSwitcher></ThemeSwitcher>
-	</footer>
-</div>
+</Tooltip.Provider>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -2,8 +2,11 @@
 	import type { PageData } from './$types';
 	import { route } from '$lib/ROUTES';
 	import { Button } from '$lib/client/components/ui/button';
+	import type { Post } from '$lib/server/db/schema/post';
 	import LoaderCircle from 'lucide-svelte/icons/loader-circle';
 	import Delete from 'lucide-svelte/icons/trash';
+	import * as Tooltip from '$lib/client/components/ui/tooltip/index.js';
+	import * as AlertDialog from '$lib/client/components/ui/alert-dialog/index.js';
 
 	export let data: PageData;
 </script>
@@ -31,15 +34,10 @@
 			{#each posts as post}
 				<div class="py-2"></div>
 				<div class="flex flex-row items-center">
-					<form method="POST" action={route('delete /posts')}>
-						<!-- TODO 1 add shadcn tooltip -->
-						<!-- TODO 1 add shadcn confirm alert dialog -->
-						<input type="hidden" name="id" value={post.id} />
-						<Button type="submit" variant="ghost" size="icon">
-							<Delete />
-						</Button>
-					</form>
+					{@render deleteButton(post)}
+
 					{post.author.username} - {post.createdAt.toLocaleString(undefined, {})}
+
 					<br />{post.content}
 				</div>
 			{/each}
@@ -48,3 +46,35 @@
 		{/await}
 	</div>
 </div>
+
+{#snippet deleteButton(post: Post)}
+	<AlertDialog.Root>
+		<AlertDialog.Trigger>
+			<Tooltip.Root disableHoverableContent>
+				<Tooltip.Content>Delete post</Tooltip.Content>
+				<Tooltip.Trigger>
+					<Button variant="ghost" size="icon">
+						<Delete />
+					</Button>
+				</Tooltip.Trigger>
+			</Tooltip.Root>
+		</AlertDialog.Trigger>
+
+		<AlertDialog.Content interactOutsideBehavior="close">
+			<AlertDialog.Header>
+				<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+				<AlertDialog.Description>
+					This action cannot be undone. This will permanently delete your post.
+				</AlertDialog.Description>
+			</AlertDialog.Header>
+
+			<AlertDialog.Footer>
+				<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<form method="POST" action={route('delete /posts')}>
+					<input type="hidden" name="id" value={post.id} />
+					<AlertDialog.Action>Delete</AlertDialog.Action>
+				</form>
+			</AlertDialog.Footer>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
+{/snippet}
